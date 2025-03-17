69984afc3af8157e989e50da43c329c63bdaef98
k9999-sn <1415272040@qq.com>
2025-03-17 12:50:58 +0800

    更新 魔王军士兵.cs

--- 光暗骑士/Assets/代码/敌人相关/魔王军士兵.cs
+++ 光暗骑士/Assets/代码/敌人相关/魔王军士兵.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+//高玮杰在此
 public class 魔王军士兵 : 敌人
 {
     protected override void Awake()
     {
         base.Awake();
         巡逻状态=new 巡逻状态();
+        Debug.log(“一样一样);
+        //高玮杰在此
     }
 }
